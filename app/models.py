c7932129466ce3d61288ad008fdc0e122ca014a7
Valentin <valentin.gallouedec@polytechnique.edu>
2019-07-03 11:16:09 -0400

    Ajout du modele

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField('date published')
+
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city = models.CharField(max_length=200)
+    inhabitants = models.IntegerField(default=0)
+
+
+
--- siteTest/settings.py
+++ siteTest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'siteTest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'djangoBDD'),
     }
 }
 
