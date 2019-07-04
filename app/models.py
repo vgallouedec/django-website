2fd77f9349fda35e3aff913e99ecc9426a4e6d6e
Valentin <valentin.gallouedec@polytechnique.edu>
2019-07-04 09:48:42 -0400

    page list country

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField('date published')
-
+    def __unicode__(self):
+        return self.name
 
 class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from app.models import Country
+from django.utils import timezone
 
 def index(request):
+    
+    russia = Country(name="France",date=timezone.now())
+    russia.save()
     return HttpResponse("Hello, world. You're at the app index.")
+
+def list(request):
+    pays = Country.objects.all()
+    return render(request,'index.html',{"pays":pays})
